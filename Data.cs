aa0b3ed6df4c496bda0826cf36ec34d8f84a9d55
Yonka <yonka2003@gmail.com>
2022-07-09 23:57:39 +0300

    Add project files.

---
+++ Data.cs
@@ -0,0 +1,24 @@
+﻿namespace Discord_RoomTemp_RPC
+{
+    internal static class Data
+    {
+        internal static class Settings
+        {
+            public const int UPDATE_INTERVAL_MS = 5000;
+        }
+
+        internal static class Tuya
+        {
+            public const string ACCESS_ID = "access_id";
+            public const string API_SECRET = "api_secret";
+            public const string DEVICE_ID = "device_id";
+        }
+
+        internal static class Discord
+        {
+            public const string CLIENT_ID = "client_id";
+            public const string IMAGE_KEY = "image_key";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿
+using com.clusterrr.TuyaNet;
+using DiscordRPC;
+using DiscordRPC.Logging;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Threading.Tasks;
+
+namespace Discord_RoomTemp_RPC
+{
+    internal class Program
+    {
+        private static DiscordRpcClient rpcClient;
+        private static TuyaApi tuyaApi;
+
+        private static void Init()
+        {
+            tuyaApi = new TuyaApi(region: TuyaApi.Region.CentralEurope, accessId: Data.Tuya.ACCESS_ID, apiSecret: Data.Tuya.API_SECRET);
+
+            rpcClient = new DiscordRpcClient(Data.Discord.CLIENT_ID)
+            {
+                Logger = new ConsoleLogger() { Level = LogLevel.Warning }
+            };
+
+            rpcClient.OnReady += (sender, e) =>
+            {
+                Console.WriteLine("Received Ready from user {0}", e.User.Username);
+            };
+
+            rpcClient.OnPresenceUpdate += (sender, e) =>
+            {
+                Console.WriteLine("Received Update! {0}", e.Presence.GetString());
+            };
+            rpcClient.Initialize();
+        }
+
+        private static async Task Main()
+        {
+            Init();
+            await UpdateTempAsync(); // start updating
+        }
+
+        private static string ExtractTemp(string getResult)
+        {
+            JObject json = JObject.Parse(getResult);
+            string temp = json["status"][0]["value"].ToString(); // status[0] => {"temp", value} => return value
+            return temp.Substring(0, temp.Length - 1) + "." + temp[temp.Length - 1]; // temp (300) => return 30.0
+        }
+
+        private static async Task UpdateTempAsync()
+        {
+            while (true)
+            {
+                string getResult = await tuyaApi.RequestAsync(TuyaApi.Method.GET, $"/v1.0/devices/{Data.Tuya.DEVICE_ID}");
+                string roomTemperature = ExtractTemp(getResult);
+
+                rpcClient.SetPresence(new RichPresence()
+                {
+                    Details = "In Room:",
+                    State = $"{roomTemperature} °C",
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = Data.Discord.IMAGE_KEY,
+                        LargeImageText = "By Yonka",
+                    }
+                });
+                await Task.Delay(Data.Settings.UPDATE_INTERVAL_MS);
+            }
+        }
+    }
+
+    internal static class MExtensions
+    {
+        internal static string GetString(this BaseRichPresence rpc)
+        {
+            return $"\n----------\nDetails: {rpc.Details}\nState: {rpc.State}\n----------\n";
+        }
+    }
+}
